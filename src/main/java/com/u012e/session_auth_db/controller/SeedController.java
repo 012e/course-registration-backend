b21971da5c7747be8cafdd3841d513a6ec455a2a
Pham Nhat Huy <huyphmnhat@gmail.com>
2024-11-13 10:27:39 +0700

    course and subject seeder (#7)
    * subject seeder
    
    * course seeder
    
    ---------
    
    Co-authored-by: ZeVnan <21521812@ms.uit.edu.vn>

--- src/main/java/com/u012e/session_auth_db/controller/SeedController.java
+++ src/main/java/com/u012e/session_auth_db/controller/SeedController.java
@@ -1,6 +1,8 @@
 package com.u012e.session_auth_db.controller;
 
+import com.u012e.session_auth_db.model.Course;
 import com.u012e.session_auth_db.model.Student;
+import com.u012e.session_auth_db.model.Subject;
 import com.u012e.session_auth_db.service.seeder.Seeder;
 import com.u012e.session_auth_db.utils.GenericResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SeedController {
 
     private final Seeder<Student> studentSeeder;
+    private final Seeder<Subject> subjectSeeder;
+    private final Seeder<Course> courseSeeder;
 
     @GetMapping("students")
     public GenericResponse<String> seedStudents(@RequestParam(defaultValue = "100") int count) {
@@ -24,6 +28,30 @@ public class SeedController {
         studentSeeder.seed(count);
         return GenericResponse.<String>builder()
                 .message("Students seeded")
+                .data(null)
+                .success(true)
+                .build();
+    }
+    @GetMapping("subjects")
+    public GenericResponse<String> seedSubjects(@RequestParam(defaultValue = "100") int count){
+        if (count < 0) {
+            throw new IllegalArgumentException("Count must be greater than 0");
+        }
+        subjectSeeder.seed(count);
+        return GenericResponse.<String>builder()
+                .message("Subjects seeded")
+                .data(null)
+                .success(true)
+                .build();
+    }
+    @GetMapping("courses")
+    public GenericResponse<String> seedCourses(@RequestParam(defaultValue = "50") int count){
+        if (count < 0) {
+            throw new IllegalArgumentException("Count must be greater than 0");
+        }
+        courseSeeder.seed(count);
+        return GenericResponse.<String>builder()
+                .message("Courses seeded")
                 .data(null)
                 .success(true)
                 .build();
---
+++ src/main/java/com/u012e/session_auth_db/service/seeder/CourseSeeder.java
@@ -0,0 +1,33 @@
+package com.u012e.session_auth_db.service.seeder;
+
+import com.u012e.session_auth_db.model.Course;
+import com.u012e.session_auth_db.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CourseSeeder implements Seeder<Course> {
+    private final CourseRepository courseRepository;
+    private final Faker faker;
+    private final List<Course> courseList;
+    @Override
+    public void seed(int count) {
+        for (int i = 0; i < count; ++i){
+            Course course = new Course();
+            int shift = faker.number().numberBetween(1, 2);
+            if (shift == 1){
+                course.setStartPeriod(faker.number().numberBetween(1, 2));
+            }
+            else{
+                course.setStartPeriod(faker.number().numberBetween(6, 7));
+            }
+            course.setEndPeriod(course.getStartPeriod() + 3);
+            course.setDayOfWeek(faker.number().numberBetween(1, 6));
+            course.setMaxParticipants(faker.number().numberBetween(10, 50));
+        }
+    }
+}
