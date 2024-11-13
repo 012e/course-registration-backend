e143f0537c78bb5d4a117bf6f568f48f2a1a8c2a
012e <huyphmnhat@gmail.com>
2024-11-13 20:01:02 +0700

    add Dependency create/patch

--- src/main/java/com/u012e/session_auth_db/repository/DependencyRepository.java
+++ src/main/java/com/u012e/session_auth_db/repository/DependencyRepository.java
@@ -2,7 +2,6 @@ package com.u012e.session_auth_db.repository;
 
 import com.u012e.session_auth_db.model.Dependency;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
