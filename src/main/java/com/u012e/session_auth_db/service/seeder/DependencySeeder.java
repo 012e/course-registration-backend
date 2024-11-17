8af499c9006adc92897f1f63faa593ab2859127c
012e <huyphmnhat@gmail.com>
2024-11-17 19:32:35 +0700

    simplify random algo
    # Conflicts:
    #	src/main/java/com/u012e/session_auth_db/service/seeder/DependencySeeder.java

--- src/main/java/com/u012e/session_auth_db/service/seeder/DependencySeeder.java
+++ src/main/java/com/u012e/session_auth_db/service/seeder/DependencySeeder.java
@@ -21,25 +21,6 @@ public class DependencySeeder implements Seeder<Dependency> {
     private final int MIN_DEPENDENCIES = 0;
     private final int MAX_DEPENDENCIES = 5;
 
-    private int randomReversedExponential(int begin, int end, double mean) {
-        if (begin >= end) {
-            throw new IllegalArgumentException("End must be greater than begin.");
-        }
-
-        double lambda = 1.0 / mean;
-
-        // Generate an exponentially distributed random value using inverse transform sampling
-        double u = random.nextDouble();
-        double exponentialValue = -Math.log(1 - u) / lambda;
-
-        // Scale and reverse the value to fit within the range [begin, end], skewing towards the upper end
-        int result = end - (int) Math.round(exponentialValue * (end - begin) / mean);
-
-        // Clamp the result within the range [begin, end]
-        result = Math.max(begin, Math.min(result, end));
-
-        return result;
-    }
 
     @Override
     public void seed(int count) {
@@ -51,7 +32,7 @@ public class DependencySeeder implements Seeder<Dependency> {
         for (int i = 1; i <= subjectCount - MAX_DEPENDENCIES; ++i) {
 
             // Get a random number of dependencies
-            var dependencyCount = randomReversedExponential(MIN_DEPENDENCIES, MAX_DEPENDENCIES, 1);
+            var dependencyCount = random.nextInt(MIN_DEPENDENCIES, MAX_DEPENDENCIES);
             var dependencyIds = random
                     .longs(i + 1, subjectCount)
                     .distinct()
