9f2aa925f4339e20894f5ac9713bd689d94b41a0
012e <huyphmnhat@gmail.com>
2024-11-26 17:41:37 +0700

    setup basic test

--- src/test/java/com/u012e/session_auth_db/SessionAuthDbApplicationTests.java
+++ src/test/java/com/u012e/session_auth_db/SessionAuthDbApplicationTests.java
@@ -1,13 +1,19 @@
 package com.u012e.session_auth_db;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.LinkedList;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
 class SessionAuthDbApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(new LinkedList<>()).isNotNull();
 	}
 
 }
