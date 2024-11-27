c08ab7603c9bde61a0b203b7ddd5f86640a8e627
012e <huyphmnhat@gmail.com>
2024-11-27 12:46:28 +0700

    add route for getting all courses

--- src/main/java/com/u012e/session_auth_db/service/CourseService.java
+++ src/main/java/com/u012e/session_auth_db/service/CourseService.java
@@ -2,13 +2,17 @@ package com.u012e.session_auth_db.service;
 
 import com.u012e.session_auth_db.dto.CreateCourseDto;
 import com.u012e.session_auth_db.dto.ResponseCourseDto;
+import com.u012e.session_auth_db.model.Course;
+
+import java.util.List;
 
 public interface CourseService {
     long createCourse(CreateCourseDto courseDto);
-
     ResponseCourseDto getCourse(long id);
-
     void deleteCourse(long id);
-
     void updateCourse(long id, CreateCourseDto courseDto);
+
+    List<Course> getAllById(List<Long> ids);
+
+    List<ResponseCourseDto> getAll();
 }
