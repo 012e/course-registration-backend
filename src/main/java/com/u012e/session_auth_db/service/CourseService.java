2ed836714b746a88e93f23cbd8110e53f7d584f1
012e <huyphmnhat@gmail.com>
2024-11-27 20:59:53 +0700

    throw illegal argument exception on course not found

--- src/main/java/com/u012e/session_auth_db/service/CourseService.java
+++ src/main/java/com/u012e/session_auth_db/service/CourseService.java
@@ -8,8 +8,11 @@ import java.util.List;
 
 public interface CourseService {
     long createCourse(CreateCourseDto courseDto);
+
     ResponseCourseDto getCourse(long id);
+
     void deleteCourse(long id);
+
     void updateCourse(long id, CreateCourseDto courseDto);
 
     List<Course> getAllById(List<Long> ids);
